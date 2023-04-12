7546554fb865417e9d22b95f49da255167c8f240
Warier <69541908+Warier@users.noreply.github.com>
2023-04-12 20:57:09 -0300

    atualizando link

--- config.php
+++ config.php
@@ -4,6 +4,6 @@ define('VIEW_FOLDER',__DIR__.SLASH.'view'.SLASH);
 define('DATA_LOCATION',__DIR__.SLASH.'data'.SLASH.'users.json');
 define('EMAIL_ADDRESS',''); //insira o endereco de email
 define('EMAIL_PASSWORD',''); //Insira a senha do email
-define('APP_URL','http://localhost:8080');
+define('APP_URL','http://0.0.0.0:8080');
 define('SECRET_IV', pack('a16','senha'));
 define('SECRET', pack('a16','senha'));
